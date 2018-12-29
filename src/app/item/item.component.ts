527e3f39f3f5507da6c0836ebe82a7076acb012b
Dimitrios Kastrinakis <kastrind@gmail.com>
2018-12-29 02:48:36 +0200

    Added selecting the side for a character and assigning them under the appropriate tab.

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { splitDepsDsl } from '@angular/core/src/view/util';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
+  @Input() character;
+
+  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAssign(side) {
+    // NOT recommended: change objects in a single place instead
+    //this.character.side = side;
+    this.sideAssigned.emit({name: this.character.name, side: side});
   }
 
 }
